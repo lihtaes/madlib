3149ef9a989be9187c1129deffa6b8bd23d46a4a
Bradley Campbell <bb.campbell606@gmail.com>
2018-12-04 13:59:22 -0700

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+      <div className='backgroundSkew'>
+        <div className='madlib-heading'>
+          <h1>Mad Libs by Bradley</h1>
+          <div className='madlib-subheading'>
+            <h2>Fill out the form below and hit 'generate'</h2>
+          </div>
+        </div>
       </div>
     );
   }
