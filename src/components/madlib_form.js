257ba26ebd2b318c71099d0fb34cc5c2d92c8c99
Bradley Campbell <bb.campbell606@gmail.com>
2018-12-04 15:39:58 -0700

    added form elements with reactstrap styling and inputs

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,9 +1,55 @@
 import React, {Component} from 'react';
+import {Col,
+        Row} from "reactstrap"
 
 
 class MadlibForm extends Component {
+    handleChange = function() {
+        console.log('handling');
+    }
     render() {
-      return <h1>Bradley's Mad Libs Form Component</h1>;
+      return (
+          <Row style={{textAlign: 'center', color: 'white'}}>
+              <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder="color" type="text" onChange={this.handleChange}></input>
+                    </Col>
+                </Row>
+              </Col>
+              <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder="color" type="text"></input>
+                    </Col>
+                </Row>
+              </Col><Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder="color" type="text"></input>
+                    </Col>
+                </Row>
+              </Col><Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder="color" type="text"></input>
+                    </Col>
+                </Row>
+              </Col>
+          </Row>
+      );
     }
   }
 
