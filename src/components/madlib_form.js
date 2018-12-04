de2ac6324016c9db5917ea3ebd5512d8894e7e1b
Bradley Campbell <bb.campbell606@gmail.com>
2018-12-04 16:15:01 -0700

    modified handle change function to accept props and set state

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,56 +1,96 @@
-import React, {Component} from 'react';
-import {Col,
-        Row} from "reactstrap"
-
+import React, { Component } from 'react';
+import {
+    Col,
+    Row
+} from 'reactstrap';
 
 class MadlibForm extends Component {
-    handleChange = function() {
-        console.log('handling');
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: ''
+        }
+
     }
+
+    handleChange = function(props) {
+        return function(event) {
+            this.setState({[props.inputTitle]: event.target.value});
+            console.log(`value for state ${props.inputTitle} is: ${this.state[props.inputTitle]}`);
+        }.bind(this);
+    }
+
     render() {
       return (
-          <Row style={{textAlign: 'center', color: 'white'}}>
-              <Col md="3" className="inputWrapper">
+        <Row style={{textAlign: 'center', color: 'white'}}>
+            <Col md="3" className="inputWrapper">
                 <Row>
                     <Col md="2">
                         <label className="greenLabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="color" type="text" onChange={this.handleChange}></input>
+                        <input placeholder='Color' type='text' onChange={this.handleChange({inputTitle: 'color'})} />
                     </Col>
                 </Row>
-              </Col>
-              <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Color</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
                 <Row>
                     <Col md="2">
                         <label className="greenLabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="color" type="text"></input>
+                        <input placeholder='Noun (Plural)' type='text' onChange={this.handleChange({inputTitle: 'pluralNoun'})} />
                     </Col>
                 </Row>
-              </Col><Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Noun (Plural)</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
                 <Row>
                     <Col md="2">
                         <label className="greenLabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="color" type="text"></input>
+                        <input placeholder='Adjective' type='text' onChange={this.handleChange({inputTitle: 'adjectiveOne'})} />
                     </Col>
                 </Row>
-              </Col><Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Adjective</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
                 <Row>
                     <Col md="2">
                         <label className="greenLabel">1</label>
                     </Col>
                     <Col md="10">
-                        <input placeholder="color" type="text"></input>
+                        <input placeholder='Celebrity' type='text' onChange={this.handleChange({inputTitle: 'celebrityOne'})} />
                     </Col>
                 </Row>
-              </Col>
-          </Row>
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Celebrity</div>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
       );
     }
-  }
+}
 
-  export default MadlibForm;
+export default MadlibForm;
